dcd804a809e1b73b563e8bf6100350ab51bce46c
Nitesh <nitzz057@gmail.com>
2023-11-17 12:24:56 +0530

    review update

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import authRoutes from "./routes/authRoutes.js"
 import reviewRoutes from "./routes/reviewRoutes.js"
 import morgan from "morgan";
 import { isAdmin, requireSignIn } from "./middlewares/authMiddleware.js";
+import path from 'path';
+import { fileURLToPath } from "url";
 
 
 dotenv.config();
@@ -14,9 +16,20 @@ connectDB();
 app.use(express.json());
 app.use(morgan("dev"))
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use(express.static(path.join(__dirname,'./client/build')))
+
 //Routes
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/review',reviewRoutes);
+
+app.use("*", function(req, res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
+  });
 
 const PORT = process.env.PORT;
 
